bbd28c75ad3d98ba57849a3bdfbe7dd1803fbbd8
mast3rkryptos <mast3rkryptos@users.noreply.github.com>
2020-12-12 00:07:05 -0600

    Day 12 done

--- AdventOfCode2020/Day12.cs
+++ AdventOfCode2020/Day12.cs
@@ -10,7 +10,6 @@ namespace AdventOfCode2020
     {
         public static void Part1()
         {
-
             Int64 x = 0, y = 0, direction = 90;
             using (var sr = new StreamReader("Day12_Input.txt"))
             {
@@ -32,14 +31,15 @@ namespace AdventOfCode2020
                             x -= Convert.ToInt64(line.Substring(1));
                             break;
                         case 'L':
-                            for (Int64 reducedDirectionChange = -(Math.Abs(Convert.ToInt64(line.Substring(1))) % 360); reducedDirectionChange != 0; reducedDirectionChange += 90)
+                            for (Int64 reducedDirectionChange = Math.Abs(Convert.ToInt64(line.Substring(1))) % 360; reducedDirectionChange != 0; reducedDirectionChange -= 90)
                             {
                                 if (direction == 0) direction = 270;
                                 else direction -= 90;
                             }
                             break;
                         case 'R':
-                            direction += Convert.ToInt64(line.Substring(1)) % 360;
+                            direction += Convert.ToInt64(line.Substring(1));
+                            direction %= 360;
                             break;
                         case 'F':
                             switch (direction)
@@ -67,7 +67,55 @@ namespace AdventOfCode2020
 
         public static void Part2()
         {
-            
+            Int64 sx = 0, sy = 0, wx = 10, wy = 1;
+            using (var sr = new StreamReader("Day12_Input.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Int64 temp = 0;
+                    switch (line[0])
+                    {
+                        case 'N':
+                            wy += Convert.ToInt64(line.Substring(1));
+                            break;
+                        case 'S':
+                            wy -= Convert.ToInt64(line.Substring(1));
+                            break;
+                        case 'E':
+                            wx += Convert.ToInt64(line.Substring(1));
+                            break;
+                        case 'W':
+                            wx -= Convert.ToInt64(line.Substring(1));
+                            break;
+                        case 'L':
+                            for (Int64 i = Convert.ToInt64(line.Substring(1)) % 360; i > 0; i -= 90)
+                            {
+                                temp = wx;
+                                wx = -wy;
+                                wy = temp;
+                            }
+                            break;
+                        case 'R':
+                            for (Int64 i = Convert.ToInt64(line.Substring(1)) % 360; i > 0; i -= 90)
+                            {
+                                temp = wx;
+                                wx = wy;
+                                wy = -temp;
+                            }
+                            break;
+                        case 'F':
+                            for (Int64 i = Convert.ToInt64(line.Substring(1)); i > 0; i--)
+                            {
+                                sx += wx;
+                                sy += wy;
+                            }
+                            break;
+                    }
+                }
+                Console.WriteLine(Math.Abs(sx) + Math.Abs(sy));
+            }
+            Console.ReadKey();
         }
     }
 }
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -27,7 +27,8 @@ namespace AdventOfCode2020
             //Day10.Part2();
             //Day11.Part1();
             //Day11.Part2();
-            Day12.Part1();
+            //Day12.Part1();
+            Day12.Part2();
         }
     }
 }
